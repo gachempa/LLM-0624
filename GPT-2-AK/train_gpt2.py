8ef1ee010e253ab6cee9b1ad21759d56339ee8c0
gachempa <jpsg514@gmail.com>
2024-06-24 08:50:31 -0700

    more to go

--- GPT-2-AK/train_gpt2.py
+++ GPT-2-AK/train_gpt2.py
@@ -1,8 +1,47 @@
+# from Anfrej Karpathy's https://www.youtube.com/watch?v=l8pRSuU81PU&t=15s
 
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import math
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self,config) -> None:
+        super().__init__()
+        assert config.n_embd % config.n_head ==0
+        # k, q, v prjections for all heads but in a batch 
+        self.c_attn=nn.Linear(config.n_embd, 3*config.n_embd)
+        # outout projection
+        self.c_proj=nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.n_head=config.n_head
+        self.n_embd=config.n_embd
+        # not really a bias but a mask, following HI naming
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1,1, config.block_size,config.block_size))
+
+    def forward(self,x):
+        B,T,C=x.size() # batch size, sequence length, embedding dimensionality (n_embd) 
+        # calculate q, k, v for all heads in batch and move head forward to be the batch ..
+        # nh is "no. of heads", hs is head size, and C (# of channels)=nh*hs
+        # eg in GPT-2 (124M), n_head=64, hs=64, so nh*hs=768 channels in the Xmer
+        qkv=self.c_attn(x)
+        q,k,v=qkv.split(self.n_embd, dim=2)
+        k=k.view(B,T,self.n_head,C//self.n_head).transport(1,2) # (B,nh,T,hs)
+        q=q.view(B,T,self.n_head,C//self.n_head).transport(1,2) # (B,nh,T,hs)  
+        v=v.view(B,T,self.n_head,C//self.n_head).transport(1,2) # (B,nh,T,hs)
+        # attention (materializes the large (T,T) matrix for all the queries and keys)
+        att=(q@k.transpose(-2,-1))*(1.0/math.sqrt(k.size(-1)))
+        att=att.masked_fill(self.bias[:,:,:T,:T]==0,float('-inf'))
+        att=F.softmax(att,dim=-1)
+        y=att@v # (B,nh,T,T) x (B,nh,T,hs) -> (B,nh,T,hs)
+        y=y.transpose(1,2).contiguous().view(B,T,C) # re-assemble all head outputs side by side
+        # output projection
+        y=self.c_proj(y)
+        return y
+
 
 class MLP(nn.Module):
 
@@ -34,11 +73,11 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int=256
-    vocab_size: int=65
-    n_layer: int=6
-    n_head: int=6
-    n_embd: int=384
+    block_size: int=1024 # max sequence length
+    vocab_size: int=50527 # no. of tokens: 50,000 BPE merges + 256 bu=ytes tokens +1 <|endoft..
+    n_layer: int=12
+    n_head: int=12
+    n_embd: int=768
 
 class GPT(nn.Module):
 
